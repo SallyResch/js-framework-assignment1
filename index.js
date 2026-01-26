a06034c068c1c806ad045ab4f50612aea356461c
Sally Resch <sallyresch27@gmail.com>
2026-01-26 11:40:58 +0100

    added console.log to every route

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { log } = require("console");
 const http = require("http");
 const url = require("url");
 
@@ -15,26 +16,38 @@ http.createServer((req, res) => {
 	let address = url.parse(req.url, true);
 	const currentPath = address.pathname;
 	const searchTerm = address.query;
+
+	console.log(address);
+	console.log(currentPath);
+	console.log(searchTerm);
+
 	res.writeHead(200, "All is Okie Dokie", { "Content-type": "text/html" })
 
 	if (currentPath === "/") {
 		res.write(navigation());
 		res.write(header("Home"))
 		res.write(footer("homefooter"));
-
+		console.log(address);
+		console.log(currentPath);
+		console.log(searchTerm);
 		res.end();
 	}
 	if (currentPath === "/warframes") {
 		res.write(navigation());
 		res.write(header("Warframes"))
 		res.write(footer("warframesfooter"));
-
+		console.log(address);
+		console.log(currentPath);
+		console.log(searchTerm);
 		res.end();
 	}
 	if (currentPath === "/about") {
 		res.write(navigation());
 		res.write(header("About"))
 		res.write(footer("aboutfooter"));
+		console.log(address);
+		console.log(currentPath);
+		console.log(searchTerm);
 		res.end();
 	}
 }).listen(3456, () => console.log("Listening on port 3456"))
