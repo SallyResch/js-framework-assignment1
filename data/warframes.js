0b148461e91203c85473acbf10a586275d273080
Sally Resch <sallyresch27@gmail.com>
2026-01-26 11:26:52 +0100

    added more data

--- data/warframes.js
+++ data/warframes.js
@@ -2,5 +2,25 @@ const warframes = [
 	{
 		name: "Volt",
 		element: "Electricity",
+		tacticalAbility: "Speed",
+		description: "./content/text/volt.txt"
+	},
+	{
+		name: "Ember",
+		element: "Electricity",
+		tacticalAbility: "Speed",
+		description: "./content/text/volt.txt"
+	},
+	{
+		name: "Nokko",
+		element: "Electricity",
+		tacticalAbility: "Speed",
+		description: "./content/text/volt.txt"
+	},
+	{
+		name: "Hilldrun",
+		element: "Electricity",
+		tacticalAbility: "Speed",
+		description: "./content/text/volt.txt"
 	}
 ]
--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const url = require("url");
 const navigation = () => {
 	return (`<nav><a href="/">Home</a><a href="/warframes">Warframes</a><a href="/about">About</a></nav>`)
 }
+const header = (headerTitle) => {
+	return (`<header><h1>${headerTitle}</h1></header>`);
+}
 
+const footer = (footerTitle) => {
+	return (`<footer><h1>${footerTitle}</h1></footer>`);
+}
 http.createServer((req, res) => {
 	let address = url.parse(req.url, true);
 	const currentPath = address.pathname;
@@ -13,14 +19,22 @@ http.createServer((req, res) => {
 
 	if (currentPath === "/") {
 		res.write(navigation());
+		res.write(header("Home"))
+		res.write(footer("homefooter"));
+
 		res.end();
 	}
 	if (currentPath === "/warframes") {
 		res.write(navigation());
+		res.write(header("Warframes"))
+		res.write(footer("warframesfooter"));
+
 		res.end();
 	}
 	if (currentPath === "/about") {
 		res.write(navigation());
+		res.write(header("About"))
+		res.write(footer("aboutfooter"));
 		res.end();
 	}
 }).listen(3456, () => console.log("Listening on port 3456"))
